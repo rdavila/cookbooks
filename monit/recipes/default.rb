2c2aad02103e5a34ae192815af00de0ef6ef5a42
Rubén Dávila Santos <rdavila84@gmail.com>
2010-08-17 10:10:39 -0500

    recipe for monit

---
+++ monit/recipes/default.rb
@@ -0,0 +1,31 @@
+case node[:platform]
+
+when 'centos'
+  yum_package "monit" do
+    action :install
+  end
+
+  cookbook_file "/etc/monit.conf" do
+    source "monit.conf"
+    mode "0644"
+  end
+when 'ubuntu', 'debian'
+  apt_package "monit" do
+    action :install
+  end
+
+  cookbook_file "/etc/default/monit" do
+    mode "0644"
+  end
+
+  cookbook_file "/etc/monit/monitrc" do
+    source "monit.conf"
+    mode "0644"
+  end
+  
+end
+
+service "monit"  do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
